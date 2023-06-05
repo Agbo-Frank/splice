d937ddfa1386e25a29577a7f01ec39da1afb39c4
Agbo Frank <agbofrank531@gmail.com>
2023-06-05 18:31:45 +0100

    made the fonts look better

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -11,7 +11,7 @@ export default function Hero(){
                     <Header />
                     <div className="absolute top-0 right-0 left-0 z-10 flex w-full h-full pl-6 md:pl-14">
                         <div className="h-full w-full lg:w-[45%] flex flex-col justify-center">
-                            <p className="hero_text leading-[70px] font-[800] text-[7vw] md:text-[5.5vw]">Where your music starts</p>
+                            <p className="hero_text md:leading-[70px] font-[800] text-[7vw] md:text-[5.5vw]">Where your music starts</p>
                             <p className="my-3 text-[#595959] font-normal text-[17px]">Royalty-free sounds. Industry-leading software. Endless inspiration. Start creating with Splice.</p>
                             <Button title="sign up" />
                         </div>
